18d01a2183283f4326c284d220e43fc1298e6e76
gabriel <oqquinayas2@gmail.com>
2023-09-11 16:58:40 -0500

    primer ejercicio

---
+++ ejercicio.js
@@ -0,0 +1,15 @@
+function guardar() {
+    var n=document.getElementById('numero1')
+    var n2=document.getElementById('numero2')
+
+    numero1=n.value
+    numero2=n2.value
+let suma=parseInt(numero1)+ parseInt(numero2)
+
+console.log(suma);
+}
+
+
+
+
+
---
+++ listas.js
@@ -0,0 +1,20 @@
+var numeros=[45,68,97,65,24]
+
+
+let frutas=['manzana','pera','kiww','papaya','banano']
+
+const lista=[1,2,3, true ,'hoola',[4,5,6]]
+
+//console.log(lista[5][2]);
+//console.log(frutas.sort())//organiza alfaveticaente
+//console.log("copia"+copia)
+//var i=0
+//while (i<lista.length) {
+//console.log(lista[i]);
+//i++    
+//}
+let  j=0
+do {
+    console.log(lista[j]);
+    j++
+} while (j<lista.length);
